a1fb681363a334a5dad431937e57f2018aa11270
Carolina Hernandez <carolina@Carolinas-MacBook-Pro.local>
2022-05-14 13:32:57 -0700

    updated js

---
+++ script.js
@@ -0,0 +1,11 @@
+var winCount= 0;
+var lossCount= 0;
+var playBtn = document.getElementsByClassName("playgame");
+var fillIN = document.getElementsByClassName ("fill-in");
+var win = document.getElementsByClassName ("win");
+var loss = document.getElementsByClassName ("lose");
+var resetBtn= document.getElementsByClassName ("reset-button");
+var timer= document.getElementsByClassName ("timer-countdown")
+
+
+
